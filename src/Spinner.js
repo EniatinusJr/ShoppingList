c7d2c0074a0c335c1e92f319236dfc4ec1400126
Pandouby <57749377+Pandouby@users.noreply.github.com>
2021-12-02 14:55:14 +0100

    added Spinner component

---
+++ src/Spinner.js
@@ -0,0 +1,22 @@
+import {Star, StarFill} from "react-bootstrap-icons";
+import {useState} from "react";
+
+export default function Rating(defautlRating) {
+    let [value, setValue] = useState(1);
+
+    function plus() {
+        setValue(value+1);
+    }
+
+    function minus() {
+        setValue(value-1);
+    }
+
+    return(
+        <> 
+            <button onClick={plus}>+</button>
+            <p>{value}</p>
+            <button onClick={minus}>-</button>
+        </>
+    )
+}
