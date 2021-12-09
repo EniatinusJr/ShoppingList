0ee3fd50eab9332a6b3bf93c3c959191f7124f99
Maurin Schucan <maurin@schucan.ch>
2021-12-09 15:43:37 +0100

    changed component call

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Row, Col, InputGroup, FormControl, Button, ListGroup} from 'react-bootstrap';
-import {} from "react-bootstrap-icons";
-import {useState, useEffect} from "react";
+import { Row, Col, InputGroup, FormControl, Button, ListGroup } from 'react-bootstrap';
+import { } from "react-bootstrap-icons";
+import { useState, useEffect } from "react";
 import ListElement from './Listelement';
 
 
 function App() {
-  
+
   let [currentValue, setCurrentValue] = useState();
   let [list, setList] = useState([]);
-  
+
 
   function addToList(name, checked) {
     let date = new Date(),
-    day = date.getDate(),
-    month = (date.getMonth() + 1 ),
-    year = date.getFullYear();
-    let item = {name, checked, timeStamp: Date.now(), date: [day, month , year].join("/")};
+      day = date.getDate(),
+      month = (date.getMonth() + 1),
+      year = date.getFullYear();
+    let item = { name, checked, timeStamp: Date.now(), date: [day, month, year].join("/") };
     let listTemp = [...list, item]
     setList(listTemp);
     localStorage.setItem("list", JSON.stringify(listTemp))
   }
 
   useEffect(
-  function loadList() {
-    if(localStorage.getItem("list") != null) {
-      setList(JSON.parse(localStorage.getItem("list")));
+    function loadList() {
+      if (localStorage.getItem("list") != null) {
+        setList(JSON.parse(localStorage.getItem("list")));
+      }
     }
-  }
-  , []
+    , []
   );
 
   function delItem(timeStamp) {
-      list.map((item) => {if(item.timeStamp === timeStamp) {
-        list.remove(item)}
-      })
+    console.log(timeStamp)
+    list.map((item) => {
+      if (item.timeStamp === timeStamp) {
+        
+      }
+    })
   }
 
   return (
@@ -43,14 +46,14 @@ function App() {
       <Row className="justify-content-md-center">
         <Col xs={7} md={4}>
           <InputGroup className="mb-3">
-            <Button variant="outline-secondary" id="button-addon1" onClick={ () => addToList(currentValue, false)}>
+            <Button variant="outline-secondary" id="button-addon1" onClick={() => addToList(currentValue, false)}>
               +
             </Button>
             <FormControl
               aria-label="Example text with button addon"
               aria-describedby="basic-addon1"
               placeholder="item"
-              onChange={ (v) => setCurrentValue(v.target.value)}
+              onChange={(v) => setCurrentValue(v.target.value)}
             />
           </InputGroup>
         </Col>
@@ -59,8 +62,8 @@ function App() {
       <Row className="justify-content-md-center">
         <Col xs={7} md={4}>
           <ListGroup>
-            {list.map((listElement) => 
-              <ListElement name={listElement.name} checked={ (v) => listElement.checked = v} date={listElement.date} timeStamp={list.timeStamp}/>
+            {list.map((listElement) =>
+              <ListElement name={listElement.name} onChange={(v) => listElement.checked = v} checked={listElement.checked} date={listElement.date} timestamp={listElement.timeStamp} onClick={() => delItem(listElement.timeStamp)} />
             )}
           </ListGroup>
         </Col>
--- src/Listelement.js
+++ src/Listelement.js
@@ -4,16 +4,16 @@ import { ListGroup, InputGroup, FormControl, Button } from "react-bootstrap";
 
 
 export default function Listelement(props) {
-    let [state, setState] = useState(false);
+    let [checked, setChecked] = useState(props.checked);
 
 
     return (
         <>
             <ListGroup.Item>
                 <InputGroup className="mb-3">
-                    <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={state} onChange={(e) => { setState(e.target.checked); props.checked(state) }}/>
+                    <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={checked} onChange={(e) => { setChecked(e.target.checked); props.onChange(checked) }}/>
                     <FormControl aria-label="Text input with checkbox" value={props.name + " " + props.date} disabled />
-                    <Button variant="outline-secondary" id="button-addon2">
+                    <Button variant="outline-secondary" id="button-addon2" onClick={() => props.onClick()}>
                         <TrashFill />
                     </Button>
                 </InputGroup>
